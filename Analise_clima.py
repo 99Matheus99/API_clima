0c8cfd081f2803bc78cf2182bb6c42daa041c0ca
matheus de oliveira <nubinhopro5@gmail.com>
2023-11-09 22:39:49 -0300

    corpo básico da request

---
+++ Analise_clima.py
@@ -0,0 +1,16 @@
+import requests
+url = 'https://api.open-meteo.com/v1/forecast?latitude=-18.729616262361755&longitude=-39.8445161110831&hourly=cloud_cover&timezone=America%2FSao_Paulo&forecast_days=3'
+response = requests.get(url)
+if response.status_code == 200: # caso ele tenha conseguido se comunicar
+    data = response.json() # guarda meus dados em um formato json
+    time = data["hourly"]["time"]
+    cloud_cover = data["hourly"]["cloud_cover"]
+    # O '[]' no meio do for significa que a cada item que adiciono, ele já sai no formato de lista
+    form_time = [t.split('T')[1] for t in time] # no tempo, divido em 2 strings a partir do caractere 'T', e depois pego o item 1 dessa string separada
+    print(form_time)
+    # Plot do gráfico
+    #     
+elif response.status_code == 404:
+    print('Não encontrado!')
+else: 
+    print('Ocorreu outro erro...')
